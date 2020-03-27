669d814d62057b192e94e1c6b5cf430527763d70
Alex Mufatti <me@alexmufatti.it>
2020-03-27 22:59:29 +0100

    improvements

--- graph/script.js
+++ graph/script.js
@@ -1,20 +1,26 @@
 import Chart from 'chart.js';
 import axios from "axios";
 import moment from "moment";
+import $ from "jquery";
+import boostrap from "bootstrap"
+import c from "chartjs-plugin-colorschemes"
 
 let myChart = null;
+let countries = null;
+let colletion = 'confirmed';
 
-function getData(response, t, country, name, province) {
-    return response.data[t].locations.find(l => l.country_code === country && l.province === province).history;
+function getData(t,idx) {
+    return countries[idx][t];
 }
 
-function getCountryData(response, country, name, province) {
-    return getData(response, 'confirmed', country, name, province)
+function getCountryData(idx) {
+    return getData(colletion, idx)
 }
 
-function createSeries(response, countryCode, offset, name, province) {
+function createSeries(offset, idx) {
     if (!offset) offset = 0;
-    let values = getCountryData(response, countryCode, name, province);
+    let values = getCountryData(idx);
+    if (values.length === 0 || Object.values(values).reduce((a,b) => a+b,0) === 0  ) return [];
     let data = [];
 
     let i = 0;
@@ -37,81 +43,54 @@ function createSeries(response, countryCode, offset, name, province) {
     return data;
 }
 
-function createChart(response) {
-    let ctx = document.getElementById('myChart');
-    let gapES = document.getElementById('ES').value;
-    let gapDE = document.getElementById('DE').value;
-    let gapFR = document.getElementById('FR').value;
-    let gapKO = document.getElementById('KO').value;
-    let gapJP = document.getElementById('JP').value;
-    let gapCH = document.getElementById('CH').value;
+function createData(response) {
+    if (countries === null) {
+        countries = response.data.confirmed.locations.map((l, idx) => {
+            return {
+                selected: l.country_code === 'IT' || (l.country_code === 'FR' && l.province === '') || l.country_code === 'DE' || l.country_code === 'CH',
+                index: idx,
+                name: l.country_code+" - " +l.province,
+                cc: l.country_code,
+                offset: 0,
+                confirmed: response.data.confirmed.locations[idx].history,
+                deaths: response.data.deaths.locations[idx].history,
+                recovered: !!response.data.recovered.locations[idx] ? response.data.recovered.locations[idx].history:[]
+            };
+        });
+        createChecks();
+    }
+}
 
-    const dataES = createSeries(response, 'ES', +gapES, '', '');
-    const dataIT = createSeries(response, 'IT', 0, '', '');
-    const dataCH = createSeries(response, 'CH', +gapCH, '', '');
-    const dataFR = createSeries(response, 'FR', +gapFR, '', '');
-    const dataDE = createSeries(response, 'DE', +gapDE, '', '');
-    const dataKO = createSeries(response, 'KR', +gapKO, '', '');
-    const dataJP = createSeries(response, 'JP', +gapJP, '', '');
-    const x = dataIT.map((d, i) => i);
+function createChart() {
+    let ctx = document.getElementById('myChart');
+    let datasets = [];
+    countries.filter(c => c.selected).forEach(c => {
+        let gap = document.getElementById('gap_'+c.index).value;
+        c.offset = gap;
+        const data = createSeries(+gap, c.index);
+        datasets.push({
+            label: c.cc,
+            data: data,
+            borderWidth: 1,
+            fill: false
+        });
+    });
+
+    const x = datasets[0].data.map((d, i) => i);
 
     if (!myChart) {
         myChart = new Chart(ctx, {
             type: 'line',
             data: {
                 labels: x,
-                datasets: [{
-                    label: 'IT',
-                    data: dataIT,
-                    borderWidth: 1,
-                    fill: false,
-                    borderColor: 'green'
-                }, {
-                    label: 'CH',
-                    data: dataCH,
-                    borderWidth: 1,
-                    fill: false,
-                    borderColor: 'red'
-                }, {
-                    label: 'FR',
-                    data: dataFR,
-                    borderWidth: 1,
-                    fill: false,
-                    borderColor: 'blue'
-                }, {
-                    label: 'DE',
-                    data: dataDE,
-                    borderWidth: 1,
-                    fill: false,
-                    borderColor: 'black',
-                    spanGaps: false
-                },
-                    {
-                        label: 'KO',
-                        data: dataKO,
-                        borderWidth: 1,
-                        fill: false,
-                        borderColor: 'purple',
-                        spanGaps: false
-                    },
-                    {
-                        label: 'JP',
-                        data: dataJP,
-                        borderWidth: 1,
-                        fill: false,
-                        borderColor: 'yellow',
-                        spanGaps: false
-                    },
-                    {
-                        label: 'ES',
-                        data: dataES,
-                        borderWidth: 1,
-                        fill: false,
-                        borderColor: 'gray',
-                        spanGaps: false
-                    }]
+                datasets: []
             },
             options: {
+                plugins: {
+                    colorschemes: {
+                        scheme: 'brewer.Paired12'
+                    }
+                },
                 responsive: false,
                 animation: {
                     duration: 0
@@ -149,68 +128,62 @@ function createChart(response) {
                 }
             }
         });
-    } else {
-        myChart.data.datasets = [{
-            label: 'IT',
-            data: dataIT,
-            borderWidth: 1,
-            fill: false,
-            borderColor: 'green'
-        }, {
-            label: 'CH',
-            data: dataCH,
-            borderWidth: 1,
-            fill: false,
-            borderColor: 'red'
-        }, {
-            label: 'FR',
-            data: dataFR,
-            borderWidth: 1,
-            fill: false,
-            borderColor: 'blue'
-        }, {
-            label: 'DE',
-            data: dataDE,
-            borderWidth: 1,
-            fill: false,
-            borderColor: 'black',
-            spanGaps: false
-        },
-            {
-                label: 'KO',
-                data: dataKO,
-                borderWidth: 1,
-                fill: false,
-                borderColor: 'purple',
-                spanGaps: false
-            },
-            {
-                label: 'JP',
-                data: dataJP,
-                borderWidth: 1,
-                fill: false,
-                borderColor: 'yellow',
-                spanGaps: false
-            },
-            {
-                label: 'ES',
-                data: dataES,
-                borderWidth: 1,
-                fill: false,
-                borderColor: 'gray',
-                spanGaps: false
-            }];
+    }
+        myChart.data.datasets = datasets;
 
         myChart.update();
-    }
+
+}
+
+function createChecks() {
+    $("#check-container").empty();
+    $("#offset_container").empty();
+    countries.forEach( l => {
+        let id = 'id_'+l.index;
+        let elem = "<div><label for='"+ id + "'>"+l.name +
+            "</label><input type='checkbox' id='"+id+"' /></div>";
+
+        $(".check-container").append(elem);
+        $("#"+id).change(checkChange);
+        $("#"+id).prop('checked', l.selected);
+
+        if (l.selected) {
+            let id_gap = 'gap_'+l.index;
+            let elemO = "<label for=\""+id_gap+"\">"+l.name + "</label><input type=\"number\" id=\""+id_gap+"\" value=\""+l.offset+"\"/>";
+
+            $("#offset_container").append(elemO);
+        }
+    });
+}
+
+function checkChange(evt) {
+    let id = evt.target.id.replace('id_', '');
+    countries[id].selected = evt.target.checked;
+    createChecks();
+    createChart();
+}
+
+function collectionChanged(evt) {
+    $('#confirmed').prop('checked', evt.target.id === 'confirmed');
+    $('#deaths').prop('checked', evt.target.id === 'deaths');
+    $('#recovered').prop('checked', evt.target.id === 'recovered');
+    colletion = evt.target.id;
+    draw();
 }
 
 function draw() {
-    axios.get('https://coronavirus-tracker-api.herokuapp.com/all')
-        .then(response => {
-            createChart(response);
-        });
+    createChart();
 }
 
-document.getElementById('btn').addEventListener('click', draw);
-draw();
+$(document).ready(function() {
+    $('#btn').click(draw);
+    $('#confirmed').change(collectionChanged);
+    $('#deaths').change(collectionChanged);
+    $('#recovered').change(collectionChanged);
+    axios.get('https://coronavirus-tracker-api.herokuapp.com/all')
+        .then(r => {
+            createData(r);
+            draw();
+        });
+});
+
